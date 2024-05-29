11d3faa70e8c87e9de10ecee483f4980b7930abd
WojRud <wojciechrudol@gmail.com>
2024-05-29 22:00:44 +0200

    fixed a bug with not sending data to the new user's database after registering

--- src/main/java/com/dailycode/MyShop/UserRepository.java
+++ src/main/java/com/dailycode/MyShop/UserRepository.java
@@ -5,13 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
- //   Optional<User> findByEmail(String email);
-
-  //  User findByEmail(String email);
-
-
-
-
-    //////////////////////////////    ////////////////////////////////////////DODANO    ////////////////////////////////////////DODANO//////////DODANO
     User findByEmail(String email);
 }
--- src/main/java/com/dailycode/MyShop/UserService.java
+++ src/main/java/com/dailycode/MyShop/UserService.java
@@ -2,13 +2,17 @@ package com.dailycode.MyShop;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 public interface UserService {
     boolean isValidUser(String email, String password);
     void registerUser(User user);
 
 
-    //////////////////////////////    ////////////////////////////////////////DODANO    ////////////////////////////////////////DODANO//////////DODANO
+
+
+
     User findByEmail(String email);
 
 }
+
